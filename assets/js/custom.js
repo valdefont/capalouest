dc700b17eaeee060441efcbd86bc87848451de4b
valdefont <34252584+valdefont@users.noreply.github.com>
2017-12-08 20:30:38 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,9 +3,19 @@ $('.top').click(function() {
   $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
 });
 $(window).scroll(function() {
-  if ($(this).scrollTop() > $(window).height()) {
+  /*if ($(this).scrollTop() > $(window).height()) {
     $('.top').addClass("up");
   } else {
     $('.top').removeClass("up");
-  }
+  }*/
+           var scroll = $(window).scrollTop();
+ 
+       if (scroll >= 550) {
+         $('.main_header').css({'width' : '150px'});
+         $('.main_header').css({'padding-top' : '5px' , 'padding-bottom' : '0px'});
+     } else{
+         $('.main_header').css({'width' : '180px' , 'height' : '82px'});
+         $('.main_header').css({'padding-top' : '40px' , 'padding-bottom' : '40px'});
+ 
+    }
 });
