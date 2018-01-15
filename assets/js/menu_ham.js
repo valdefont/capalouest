ad5a8d73b797dff2998c089cdbef603824aa0a04
Valdefont <jordi_cuchet@hotmail.com>
2018-01-15 20:55:09 +0100

    message du commit

--- assets/js/menu_ham.js
+++ assets/js/menu_ham.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
+$('.clickzone').hide();
   $(document).delegate('.open', 'click', function(event){
     $(this).addClass('oppenned');
+    $('.clickzone').show();
     event.stopPropagation();
   })
   $(document).delegate('body', 'click', function(event) {
     $('.open').removeClass('oppenned');
+    $('.clickzone').hide();
   })
-  $(document).delegate('.cls', 'click', function(event){
+  $(document).delegate('.clickzone', 'click', function(event){
     $('.open').removeClass('oppenned');
+    $('.clickzone').hide();
     event.stopPropagation();
   });
 });
