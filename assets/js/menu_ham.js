c40aa8e5467884db2434606c11f6267885646443
Valdefont <jordi_cuchet@hotmail.com>
2018-02-12 21:47:48 +0100

    message du commit

--- assets/js/menu_ham.js
+++ assets/js/menu_ham.js
@@ -3,16 +3,19 @@ $('.clickzone').hide();
   $(document).delegate('.open', 'click', function(event){
     $(this).addClass('oppenned');
     $('.clickzone').show();
+    $('.DivMenu').hide();
     $('.open').css("cssText", "opacity: 1.0 !important;");
     event.stopPropagation();
   })
   $(document).delegate('body', 'click', function(event) {
     $('.open').removeClass('oppenned');
     $('.clickzone').hide();
+     $('.DivMenu').show();
   })
   $(document).delegate('.clickzone', 'click', function(event){
     $('.open').removeClass('oppenned');
     $('.clickzone').hide();
+    $('.DivMenu').show();
     event.stopPropagation();
   });
 });
